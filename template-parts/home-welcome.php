1c280c898c0ad3df8298b6eb4a11a317ef1f9989
hoodini47 <msajdera@gmail.com>
2021-01-07 15:01:11 +0100

    Added: BG images at section-welcome.

--- template-parts/home-welcome.php
+++ template-parts/home-welcome.php
@@ -1,4 +1,10 @@
-<section class="section-welcome">
+<section class="section-welcome" style="
+  background:url(<?php echo get_field('background_image_2') ?>), linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)),url(<?php echo get_field('background_image_1') ?>);
+ background-size: 750px, 100%, cover;
+ background-repeat: no-repeat;
+
+ 
+ background-color: #e1e1e1;background-position: right bottom;">
     <div class="wrapper-flex-column">
         <div class="wrapper-flex-column">
             <p>RESULTS FOCUSED</p>
